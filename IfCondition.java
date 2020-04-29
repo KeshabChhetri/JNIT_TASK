c3a415b9cf3612dd8a8d9fd00f1c2a47a47a05dc
keeshab <kchhetry555@gmail.com>
2020-04-29 15:59:30 -0500

    If Condition Only

---
+++ IfCondition.java
@@ -0,0 +1,11 @@
+/* Implementing if condition only */
+
+public class IfCondition{
+    public static void main (String args[]){
+        int a = 100; 
+        int b = 200;
+        if(b > a)
+        System.out.println("Larger value is: " + b );
+        
+    }
+}
