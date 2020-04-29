76249dfc8e9118bd212ff84360e88eb3f55b1ed8
keeshab <kchhetry555@gmail.com>
2020-04-29 15:58:37 -0500

    Implementing different assignment Operator

---
+++ ArithmeticOperator.java
@@ -0,0 +1,14 @@
+/* Implementing various Arithmetic operator */
+
+public class ArithmeticOperator{
+    public static void main (String args[]){
+        int a,b;
+        a = 10;
+        b = 20;
+        System.out.println("Sum is: " +  (a+b));
+        System.out.println("Difference is: " +  (b - a));
+        System.out.println("Product is: " +  (a*b));
+        System.out.println("Quotient is: " +  (b/a));
+        System.out.println("Remainder is: " +  (a%b)); // Used Modulus Operator %
+    }
+}
