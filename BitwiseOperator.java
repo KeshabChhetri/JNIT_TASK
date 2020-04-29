fc4f329f6d4171924ac231d067eabb58372c2db8
keeshab <kchhetry555@gmail.com>
2020-04-29 15:55:22 -0500

    Implementing Bitwise Operator

---
+++ BitwiseOperator.java
@@ -0,0 +1,16 @@
+/* Implementing various Bitwise operator */
+
+public class BitwiseOperator{
+    public static void main (String args[]){
+        int a,b, result;
+        a = 12;
+        b = 13;
+        
+        System.out.println("Bitwise OR: " +  (a | b));
+      
+        System.out.println("Bitwise AND: " +  (a & b));
+        
+        System.out.println("Bitwise XOR: " +  (a ^ b));
+        
+    }
+}
