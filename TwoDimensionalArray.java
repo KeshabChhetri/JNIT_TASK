4c435ee88323df2713e6aa5c7fd724a52cd71799
keeshab <kchhetry555@gmail.com>
2020-05-03 22:12:19 -0500

    2D Array

---
+++ TwoDimensionalArray.java
@@ -0,0 +1,8 @@
+public class TwoDimensionalArray
+{
+	public static void main(String[] args) {
+	    int [] [] firstarray  = {{1,2,3}, {4,5,6}} ;
+		System.out.println(firstarray[0][2]  );
+			System.out.println( firstarray[1][1] );
+	}
+}
